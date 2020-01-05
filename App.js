beb9d18b733099b642612e951109560b13648ccf
Kavit <kavitmht@gmail.com>
2020-01-05 01:16:56 -0800

    got numbers and operations to display

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 export default class App extends Component {
@@ -23,11 +24,41 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.result}></View>
-        <View style={styles.calculation}></View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>11*11</Text>
+        </View>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>121</Text>
+        </View>
         <View style={styles.buttons}>
-          <View style={styles.numbers}></View>
-          <View style={styles.operations}></View>
+          <View style={styles.numbers}>
+              <View style={styles.row}>
+                <Button title="0" />
+                <Button title="0" />
+                <Button title="0" />
+              </View>
+              <View style={styles.row}>
+                <Button title="0" />
+                <Button title="0" />
+                <Button title="0" />
+              </View>
+              <View style={styles.row}>
+                <Button title="0" />
+                <Button title="0" />
+                <Button title="0" />
+              </View>
+              <View style={styles.row}>
+                <Button title="0" />
+                <Button title="0" />
+                <Button title="0" />
+              </View>
+          </View>
+          <View style={styles.operations}>
+            <Button title="+" />
+            <Button title="+" />
+            <Button title="+" />
+            <Button title="+" />
+          </View>
         </View>
       </View>
     );
@@ -38,13 +69,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  resultText: {
+    fontSize: 30,
+    color: 'white'
+  },
+  calculationText: {
+    fontSize: 24,
+    color: 'white'
+  },
   result: {
-    flex: 2, // result takes 20% of the screen
-    backgroundColor: 'red'
+    flex: 1, // result takes 10% of the screen
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
   },
   calculation: {
-    flex: 1, // calculation takes 10% of the screen
-    backgroundColor: 'green'
+    flex: 2, // calculation takes 20% of the screen
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
   },
   buttons: {
     flex: 7, // flex of 7 takes 70% of the screen
@@ -54,8 +97,15 @@ const styles = StyleSheet.create({
     flex: 3,
     backgroundColor: 'yellow'
   },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
   operations: {
     flex: 1,
+    justifyContent: 'space-around',
     backgroundColor: 'black'
   }
 });
